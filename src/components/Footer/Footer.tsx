335db2c51b932f5b55cdd0956326f78f0dae3035
Damokless <damokless@protonmail.com>
2023-10-26 12:52:47 +0200

    feat : create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,85 @@
+"use client";
+import logo from "@/assets/images/white_logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="absolute bg-transparant md:px-[75px] mt-64  w-full">
+      <div className=" px-4 pb-6 pt-16 sm:px-6 lg:px-8 lg:pt-24">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
+          <div>
+            <div className="flex justify-center sm:justify-start items-center">
+              <Image src={logo} alt="Rudy Musiani logo" className="w-12" />
+              <h1 className="text-2xl font-bold text-white">
+                Rudy Musiani Coaching
+              </h1>
+            </div>
+
+            <p className="mt-3 max-w-md text-center text-sm leading-relaxed text-white/50 sm:max-w-xs sm:text-left">
+              Optez pour un suivi de fitness personnalisé et atteignez vos
+              objectifs.
+            </p>
+
+            <ul className="mt-8 flex justify-center gap-6 sm:justify-start md:gap-8">
+              <li>
+                <Link
+                  href="https://www.instagram.com/rudy_musiani/"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-blue-500 transition hover:text-blue-700"
+                >
+                  <span className="sr-only">Instagram</span>
+                  <svg
+                    className="h-6 w-6"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </Link>
+              </li>
+
+              <li>
+                <Link
+                  href="mailto:rudymusiani.coaching@gmail.com"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-blue-500 transition hover:text-blue-700 flex items-center gap-1.5"
+                >
+                  <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
+                    </svg>
+                    <p className="text-xs text-white">rudymusiani.coaching@gmail.com</p>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="mt-12 border-t border-white/50 pt-6">
+          <div className="text-center sm:flex sm:justify-center">
+            <p className="mt-4 text-sm text-white/50 sm:order-first sm:mt-0">
+              &copy; {new Date().toLocaleDateString('fr-fr', { year:"numeric" }) } Rudy Musiani Coaching
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
