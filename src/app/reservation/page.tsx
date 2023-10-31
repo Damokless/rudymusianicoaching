08effdf5978c906a277965e2a06a26e5b34c636b
Damokless <damokless@protonmail.com>
2023-10-31 19:30:26 +0100

    feat : adding states and formulas

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -1,4 +1,24 @@
+'use client'
+import { useState } from 'react';
+
 export default function Reservation() {
+  const [plan, setPlan] = useState("")
+  const [fullName, setFullName] = useState("")
+  const [mail, setMail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [age, setAge] = useState("")
+  const [objectifs, setObjectifs] = useState("")
+  const [pastSports, setPastSports] = useState("")
+  const [body, setBody] = useState("")
+  const [dispos, setDispos] = useState("")
+  const [diseases, setDiseases] = useState("")
+  const [journey, setJourney] = useState("")
+  const [alergies, setAlergies] = useState("")
+  const [week, setWeek] = useState("")
+  const [complements, setComplements] = useState("")
+  const [materiel, setMateriel] = useState("")
+  const [lifestyle, setLifeStyle] = useState("")
+  const [options, setOptions] = useState("")
   return (
     <div className="min-h-screen p-6 flex items-center justify-center text-white pt-[150px]">
       <div className="container max-w-screen-xl mx-auto">
@@ -27,7 +47,9 @@ export default function Reservation() {
                       placeholder="John Doe"
                       name="full_name"
                       id="full_name"
-                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 text-gray-800"
+                      value={fullName}
+                      onChange={(e) => setFullName(e.target.value)}
                     />
                   </div>
 
@@ -37,8 +59,10 @@ export default function Reservation() {
                       type="text"
                       name="email"
                       id="email"
-                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 text-gray-800"
                       placeholder="john.doe@exemple.com"
+                      value={mail}
+                      onChange={(e) => setMail(e.target.value)}
                     />
                   </div>
 
@@ -48,8 +72,10 @@ export default function Reservation() {
                       type="text"
                       name="phone"
                       id="phone"
-                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 text-gray-800"
                       placeholder="0123456789"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
                     />
                   </div>
 
@@ -66,8 +92,10 @@ export default function Reservation() {
                       type="text"
                       name="age"
                       id="age"
-                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
+                      className="h-10 border mt-1 rounded px-4 w-full bg-gray-50 text-gray-800"
                       placeholder="165cm/70kg/36"
+                      value={age}
+                      onChange={(e) => setAge(e.target.value)}
                     />
                   </div>
 
@@ -86,6 +114,8 @@ export default function Reservation() {
                         id="objectifs"
                         placeholder="Gain de muscle"
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={objectifs}
+                        onChange={(e) => setObjectifs(e.target.value)}
                       />
                     </div>
                   </div>
@@ -104,6 +134,8 @@ export default function Reservation() {
                         id="pastSports"
                         placeholder="Natation"
                         className="px-4 pt-1  resize-y appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={pastSports}
+                        onChange={(e) => setPastSports(e.target.value)}
                       />
                     </div>
                   </div>
@@ -125,6 +157,8 @@ export default function Reservation() {
                         id="body"
                         placeholder="Les bras"
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
                       />
                     </div>
                   </div>
@@ -144,6 +178,8 @@ export default function Reservation() {
                         id="dispos"
                         placeholder="du lundi au vendredi, 1H, le soir"
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={dispos}
+                        onChange={(e) => setDispos(e.target.value)}
                       />
                     </div>
                   </div>
@@ -161,6 +197,8 @@ export default function Reservation() {
                         id="diseases"
                         placeholder="Tendinites"
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={diseases}
+                        onChange={(e) => setDiseases(e.target.value)}
                       />
                     </div>
                   </div>
@@ -183,6 +221,8 @@ export default function Reservation() {
                         id="journey"
                         placeholder="Je mange 6 fois par jours"
                         className="px-4 pt-1 h-32 resize-y appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={journey}
+                        onChange={(e) => setJourney(e.target.value)}
                       />
                     </div>
                   </div>
@@ -201,6 +241,8 @@ export default function Reservation() {
                         id="alergies"
                         placeholder="Gluten ..."
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={alergies}
+                        onChange={(e) => setAlergies(e.target.value)}
                       />
                     </div>
                   </div>
@@ -220,6 +262,8 @@ export default function Reservation() {
                         id="week"
                         placeholder="Le lundi je fais le dos ..."
                         className="px-4 pt-1 h-32 resize-y appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={week}
+                        onChange={(e) => setWeek(e.target.value)}
                       />
                     </div>
                   </div>
@@ -240,6 +284,8 @@ export default function Reservation() {
                         id="complements"
                         placeholder="Protéine en poudre"
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={complements}
+                        onChange={(e) => setComplements(e.target.value)}
                       />
                     </div>
                   </div>
@@ -257,6 +303,8 @@ export default function Reservation() {
                         id="materiel"
                         placeholder="Dans la salle j'ai à ma disposition des elastiques, des haltères ..."
                         className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={materiel}
+                        onChange={(e) => setMateriel(e.target.value)}
                       />
                     </div>
                   </div>
@@ -275,6 +323,8 @@ export default function Reservation() {
                         id="lifestyle"
                         placeholder="Je me réveille à 8h ..."
                         className="px-4 pt-1 h-32 resize-y appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={lifestyle}
+                        onChange={(e) => setLifeStyle(e.target.value)}
                       />
                     </div>
                   </div>
@@ -289,14 +339,28 @@ export default function Reservation() {
                         id="options"
                         placeholder="Je n'ai jamais fait de sport"
                         className="px-4 pt-1 h-32 resize-y appearance-none outline-none text-gray-800 w-full bg-transparent"
+                        value={options}
+                        onChange={(e) => setOptions(e.target.value)}
                       />
+                    </div>
+                  </div>
+
+                  <div className="md:col-span-5 mb-6 text-center">
+                    <h1>Choisis la formule que tu veux</h1>
+                    <div className="flex justify-center gap-8 mt-6">
+                      <button onClick={() => setPlan('one_month')} className={`inline-block rounded-lg border border-blue-500 ${plan === 'one_month' ? 'bg-blue-500' : 'bg-none'} px-12 py-3 text-sm text-center font-medium text-white`}>
+                        1 mois
+                      </button>
+                      <button onClick={() => setPlan('three_month')} className={`inline-block rounded-lg border border-blue-500 ${plan === 'three_month' ? 'bg-blue-500' : 'bg-none'} px-12 py-3 text-sm text-center font-medium text-white`}>
+                        3 mois
+                      </button>
                     </div>
                   </div>
 
                   <div className="md:col-span-5 mb-6 text-right mt-5">
                     <div className="inline-flex items-end">
                       <button className="inline-block rounded-lg bg-blue-500 px-12 py-3 text-sm font-medium text-center text-white hover:bg-blue-700">
-                        Soumettre
+                        PAYER
                       </button>
                     </div>
                   </div>
