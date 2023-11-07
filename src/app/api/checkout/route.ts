e12e84db0607f7a67945e60ed9cb31c8cb4bd572
Damokless <damokless@protonmail.com>
2023-11-07 21:56:54 +0100

    feat : adding one key to metadata in checkout call

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -5,6 +5,7 @@ const stripe = require('stripe')(process.env.API_KEY_STRIPE_SECRET);
 export async function POST(req : Request) {
   const { plan, fullName, mail, phone, age, objectifs, pastSports, body, dispos, diseases, journey, alergies, week, complements, materiel, lifestyle, options} = await req.json()
   const PRICE_ID = plan === 'ONE_MONTH' ? process.env.ONE_MONTH_PLAN_STRIPE_ID : process.env.THREE_MONTH_PLAN_STRIPE_ID
+  const formule = plan === 'ONE_MONTH' ? '1 mois' : '3 mois'
 
   const session = await stripe.checkout.sessions.create({
     line_items: [
@@ -30,7 +31,8 @@ export async function POST(req : Request) {
       complements,
       materiel,
       lifestyle,
-      options
+      options,
+      formule
     },
     mode: 'payment',
     invoice_creation: {
