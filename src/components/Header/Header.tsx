713f8a3f47859d69483f66a78ee48de4768e0874
damokless <damokless@protonmail.com>
2023-11-10 00:49:03 +0100

    fix : responsive header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,11 @@ export default function Header() {
   return (
     <header className="absolute bg-transparant md:px-[75px] w-full">
       <div className="flex h-16  items-center gap-8 px-4 sm:px-6 lg:px-8">
-        <Link className="flex items-center" href="/">
+        <Link className="flex items-center w-full justify-center lg:justify-start lg:w-auto" href="/">
           <Image src='/assets/images/white_logo.png' width={48} height={48} alt="Rudy Musiani logo" className="w-12" />
           <p className="text-white">Rudy Musiani Coaching</p>
         </Link>
-        <div className="flex flex-1 items-center justify-end md:justify-center">
+        <div className="hidden md:flex flex-1 items-center justify-end md:justify-center">
           <nav aria-label="Global" className="hidden md:block">
             <ul className="flex items-center gap-6 text-sm">
               <li>
